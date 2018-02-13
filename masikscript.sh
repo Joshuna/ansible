25a21c812a58d16926a091f8df23abb9c2c638e4
Jozsef Kovacs <33051586+Joshuna@users.noreply.github.com>
2018-02-13 10:08:51 +0100

    masikscript.sh
    full one click install (in theory)

---
+++ masikscript.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install ansible.io  -y
+sudo apt-get install git
+cd ansible
+git init
+git clone https://github.com/andreipak/wordpress-ansible.git
+cd wordpress-ansible
+sudo ansible playbook -i hosts playbook.yml
