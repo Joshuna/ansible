e3789bec4347cb0c7603ce1a6a64567392c2669e
Jozsef Kovacs <33051586+Joshuna@users.noreply.github.com>
2018-02-13 10:40:31 +0100

    Update masikscript.sh

--- masikscript.sh
+++ masikscript.sh
@@ -6,8 +6,7 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible.io  -y
 sudo apt-get upgrade
-cd ansible
-git init
 git clone https://github.com/andreipak/wordpress-ansible.git
+cd ansible
 cd wordpress-ansible
 sudo ansible-playbook -i hosts playbook.yml
